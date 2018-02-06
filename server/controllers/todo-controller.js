26e0f56bcb930915d5bed31144b7faf9e39f2829
André Martingo <andremartingo@Andres-MacBook-Pro.local>
2018-02-06 12:28:42 +0000

    create documentation to Create function at todo controller

--- server/controllers/todo-controller.js
+++ server/controllers/todo-controller.js
@@ -1,5 +1,10 @@
 var { Todo } = require("../models/todo");
 
+/**
+ * Create and Add Note
+ * @param {request object} req 
+ * @param {response object} res 
+ */
 exports.create = (req, res) => {
   var todo = new Todo({
     text: req.body.text

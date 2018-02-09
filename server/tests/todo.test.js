9f4ef6a2a1013d110710fba386c17364617266d4
André Martingo <andremartingo@Andres-MacBook-Pro.local>
2018-02-09 12:25:01 +0000

    create seed file to DB

--- server/tests/todo.test.js
+++ server/tests/todo.test.js
@@ -4,28 +4,9 @@ const { ObjectID } = require("mongodb");
 
 const { app } = require("../server.js");
 const { Todo } = require("../models/todo");
+const { todos, populateTodos } = require("./seed/seed");
 
-const todos = [
-  {
-    _id: new ObjectID(),
-    text: "First Todo",
-    completed: false
-  },
-  {
-    _id: new ObjectID(),
-    text: "Second Todo",
-    completed: "333",
-    completedAt: new Date().getTime()
-  }
-];
-
-beforeEach(done => {
-  Todo.remove({}).then(() => {
-    Todo.insertMany(todos).then(() => {
-      done();
-    });
-  });
-});
+beforeEach(populateTodos);
 
 describe("POST /todos", () => {
   it("should create a new todo", done => {
